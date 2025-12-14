614a286913eb47e27f839b1f871c6c51b8d01203
sho-31 <shourya31veer@gmail.com>
2025-12-14 22:33:33 +0530

    Background sound embeded

--- litfest/src/App.jsx
+++ litfest/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Landing from "./pages/Landing";
+import AudioPlayer from "./components/AudioPlayer/AudioPlayer";
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <AudioPlayer />
       <Landing />
     </div>
   );
