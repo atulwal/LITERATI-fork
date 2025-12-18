8caa5020f2112474c966216f3d4d9e1f92bdf803
Naman Rangari <rangarinaman@gmail.com>
2025-12-18 21:32:17 +0530

    Updated the form management and submition

--- litfest/src/components/Registrarion Form/Book.jsx
+++ litfest/src/components/Registrarion Form/Book.jsx
@@ -2,12 +2,38 @@ import React, { useEffect, useRef, useState } from 'react'
 import './Book.css'
 
 const Book = () => {
+  const [formData, setFormData] = useState({
+  firstName: "",
+  lastName: "",
+  phone: "",
+  college: "",
+  year: "",
+  major: "",
+  city: "",
+  state: ""
+});
+
+  const handleChange = (e) => {
+  const { name, value } = e.target;
+  setFormData(prev => ({
+    ...prev,
+    [name]: value
+  }));
+};
+
+  const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(formData);
+};
+
+
 
   
   
 
   return (
     <div className='max-w-screen h-screen  flex justify-center items-center'>
+      <img src="src\assets\1213.gif" alt="cave gif" />
       <div className='absolute h-[80%] flex justify-center items-center '>
         <img
           src="src\assets\slazzer-preview-npzbp.svg"
@@ -26,7 +52,9 @@ const Book = () => {
       
       aspect-[510.92/375.32] flex'>
       
-      <form className="
+      <form 
+      onSubmit={handleSubmit}
+      className="
       relative
       w-full
       h-full
@@ -49,36 +77,54 @@ const Book = () => {
             <div>Enter Your Name </div>
             <input
               type="text"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
               placeholder="First Name"
               className=" rounded border border-gray-300 "
             />
 
             <input
               type="text"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}
               placeholder="Last Name"
               className=" rounded border border-gray-300"
             />
             <div>Enter Your Phone Number  </div>
             <input
               type="number"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
               placeholder=""
               className=" rounded border border-gray-300"
             />
             <div>College/School Name</div>
             <input
               type="text"
+              name="college"
+              value={formData.college}
+              onChange={handleChange}
               placeholder="Enter full name of your College/School "
               className=" rounded border border-gray-300"
             />
             <div>College Year/ Class</div>
             <input
               type="text"
+              name="year"
+              value={formData.year}
+              onChange={handleChange}
               placeholder="Enter full name of your College/School "
               className=" rounded border border-gray-300"
             />
             <div>Major (NA for School Students)</div>
             <input
               type="text"
+              name="major"
+              value={formData.major}
+              onChange={handleChange}
               placeholder="Enter Major "
               className=" rounded border border-gray-300"
             />
@@ -91,12 +137,18 @@ const Book = () => {
             <div>Town/ City</div>
             <input
               type="text"
+              name="city"
+              value={formData.city}
+              onChange={handleChange}
               placeholder="Enter Town Name/ City Name "
               className=" rounded border border-gray-300"
             />
             <div>State</div>
             <input
               type="text"
+              name="state"
+              value={formData.state}
+              onChange={handleChange}
               placeholder="Enter Your State Name "
               className=" rounded border border-gray-300"
             />
