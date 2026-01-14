e63478325c590bbc012a40a06185987df708d2a2
sho-31 <shourya31veer@gmail.com>
2026-01-14 11:34:50 +0530

    routing issue fixed

--- src/pages/publicLB.jsx
+++ src/pages/publicLB.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import LeaderboardProxy from "./LeaderboardProxy";
-import Leaderboard from "../components/Dashboard/public_leaderboard.jsx";
+import Leaderboard from "../components/Dashboard/PublicLB.jsx";
 import forest from "../assets/forest.jfif";
 import "../styles/Dashboard.css";
 import Profile from "./Profile";
