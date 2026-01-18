37c37e859bfa5340b1cfe04ce37a0ae44371630f
Gargee <gargeedhale07@gmail.com>
2026-01-18 17:13:12 +0530

    Add video background to Sponsors component

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import "./Sponsors.css";
+import sponsorbg from "../../assets/sponsorbg.mp4";
 
 const Sponsors = () => {
   return (
     <section id="sponsors" className="sponsors">
+              <video
+                className="sponsors-bg-video"
+                src={sponsorbg}
+                autoPlay
+                loop
+                muted
+                playsInline
+                preload="auto"
+              />
+            
+              <div className="sponsors-overlay"> </div>
+             <div className="sponsors-content">
       <h2>Our Sponsors</h2>
       <p>Coming Soon</p>
+      </div>
     </section>
   );
 };
